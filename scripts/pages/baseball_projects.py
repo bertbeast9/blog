fd7c4ca6d4dd87e91f6962088247a6659a793ac6
bertbeast9 <sam.bertelson@gmail.com>
2026-01-20 20:17:48 -0600

    Added first set of LaTex and hyperlinks. Now to add images

--- scripts/pages/baseball_projects.py
+++ scripts/pages/baseball_projects.py
@@ -16,4 +16,20 @@ st.write("Okay, let's start with the problem at hand. A begin decision before ea
 "outcomes will be further expanded to include sac flys, double plays, etc. The full " \
 "state-space of this simple problem is the union of the *transient* and *absorbing* " \
 "state-spaces.")
+st.latex(r'''
+    x_{k} \in S = \{(b,s) | b \in \{0,1,2,3 \}, s \in \{0,1,2 \} \} \cup \{\text{out,walk,hit by pitch, single, double, triple, home run} \}
+    ''')
+st.latex(r'''
+    x_{k} \in S = S_{t} \cup S_{a}         
+''')
+st.latex(r'''
+    |S| = 12 + 7 = 19         
+''')
+st.write("The control at our disposal is just the pitch and location that we call to " \
+"be thrown by the pitcher. Another assumption made by this model formulation is that " \
+"the pitcher has perfect command: if a 4-Seam Fastball (FF) is called in the 7 zone, " \
+"then that is exactly the pitch that is thrown. Later, we aim to incorporate more " \
+"uncertainty in the pitch location (i.e. model a pitcher with less command of their " \
+"pitches). The pitch locations are defined as zones according to " \
+"[Baseball Savant](%s) and is depicted in Figure 1." % "https://baseballsavant.mlb.com/")
 
