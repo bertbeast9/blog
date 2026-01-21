50491b025190dce512151effdd6ce0a41ce3e581
bertbeast9 <sam.bertelson@gmail.com>
2026-01-20 20:00:09 -0600

    Test!

--- scripts/pages/baseball_projects.py
+++ scripts/pages/baseball_projects.py
@@ -4,6 +4,6 @@ st.header("Baseball Projects",divider=True)
 st.sidebar.markdown("# Baseball Projects")
 st.sidebar.subheader("Pitch Solver (Count Only)")
 st.sidebar.subheader("Pitch Solver (Expanded)")
-st.subheader("Pitch Solver (Count Only)")
+st.subheader("Pitch Solver (Count Only@)")
 
 
