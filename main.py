172bc2d57df1f3c9314e1bde7359676ce4e8590a
bertbeast9 <sam.bertelson@gmail.com>
2026-01-21 16:35:53 -0600

    Updated

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 if __name__ == "__main__":
     # Define the pages
-    main_page = st.Page("./pages/home_page.py", title="Home Page", icon="🏠")
-    resume = st.Page("./pages/resume.py", title="Resume", icon="👨‍🎓")
-    about_me = st.Page("./pages/about_me.py", title="About Me", icon="👨")
-    cv = st.Page("./pages/cv.py", title="CV", icon="📖")
-    baseball_projects = st.Page("./pages/baseball_projects.py",title="Baseball Projects", icon="⚾")
+    main_page = st.Page("./scripts/pages/home_page.py", title="Home Page", icon="🏠")
+    resume = st.Page("./scripts/pages/resume.py", title="Resume", icon="👨‍🎓")
+    about_me = st.Page("./scripts/pages/about_me.py", title="About Me", icon="👨")
+    cv = st.Page("./scripts/pages/cv.py", title="CV", icon="📖")
+    baseball_projects = st.Page("./scripts/pages/baseball_projects.py",title="Baseball Projects", icon="⚾")
 
     # Set up navigation
     pg = st.navigation([main_page, baseball_projects, resume, cv, about_me])
