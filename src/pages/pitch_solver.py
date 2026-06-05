dd010b001c126064da90946dd0e591d7280fb873
bertbeast9 <sam.bertelson@gmail.com>
2026-06-05 14:15:43 -0500

    Updated pitch solver header

--- src/pages/pitch_solver.py
+++ src/pages/pitch_solver.py
@@ -844,8 +844,8 @@ def visualize_pitch_policy_count_only(state,opt_policy,split):
 
 # functions
 
-st.header("Baseball Projects",divider=True)
-st.sidebar.markdown("# Baseball Projects")
+st.header("Pitch Solver",divider=True)
+st.sidebar.markdown("# Pitch Solver")
 st.sidebar.subheader("Pitch Solver (Count Only)")
 st.sidebar.subheader("Pitch Solver (Expanded)")
 st.subheader("Pitch Solver (Count Only)")
