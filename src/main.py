c7978957fcb370bf37205544bbafd2feb570ef41
bertbeast9 <sam.bertelson@gmail.com>
2026-06-05 14:32:29 -0500

    Added home page

--- src/main.py
+++ src/main.py
@@ -2,16 +2,13 @@ import streamlit as st
 
 if __name__ == "__main__":
     # Define the pages
-    main_page = st.Page("./pages/home_page.py", title="Home Page", icon="🏠")
+    home_page = st.Page("./pages/home_page.py", title="Home Page", icon="🏠")
     about_me = st.Page("./pages/about_me.py", title="About Me", icon="👨")
-    # cv = st.Page("./pages/cv.py", title="CV", icon="📖")
     pitch_solver = st.Page("./pages/pitch_solver.py",title="Pitch Solver", icon="⚾")
-    # live_baseball_game_solver_personal = st.Page("./pages/live_baseball_game_solver_personal.py",title="Live Baseball Game Solver (My Model)", icon="⚾")
     live_baseball_game_solver_average = st.Page("./pages/live_baseball_game_solver_average.py",title="Live Baseball Average Game Solver", icon="⚾")
-    # semiconductor_projects = st.Page("./pages/semiconductor_projects.py",title="Semiconductor Projects", icon="💻")
 
     # Set up navigation
-    pg = st.navigation([pitch_solver, live_baseball_game_solver_average, about_me])#main_page, semiconductor_projects, , live_baseball_game_solver_personal
+    pg = st.navigation([home_page, pitch_solver, live_baseball_game_solver_average, about_me])
 
     # Run the selected page
     pg.run()
--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -2,10 +2,10 @@ import streamlit as st
 import json
 # import sys
 # st.write(sys.path)
-st.header ("Home Page", divider=True)
+st.header ("", divider=True)
 st.sidebar.markdown("# Home Page")
 st.subheader("Hey y'all!",divider=True)
-st.text("My name is Sam Bertelson, and welcome to my blog. This page is where I'll be keeping track of all my main and side projects.\nHopefully, you will enjoy!\n")
+st.text("My name is Sam Bertelson, and welcome to my blog. This page is where I'll be keeping track of all my main and side projects.\nHopefully, you will enjoy!\nFeel free to email me with any inquiries at sam.bertelson@gmail.com\n")
 
 
 
